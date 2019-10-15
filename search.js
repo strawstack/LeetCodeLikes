ff0c39a9ccf26301199f49a8e8b70b66d5977d2d
Richard <rhshadowgreen@gmail.com>
2019-10-15 16:56:10 -0400

    filter out failed promises

--- search.js
+++ search.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let request = require('graphql-request').request
 
+// Build the query
 const query = `query questionData($titleSlug: String!) {
   question(titleSlug: $titleSlug) {
     questionId
@@ -14,6 +15,7 @@ const query = `query questionData($titleSlug: String!) {
   }
 }`;
 
+// Flag rejected promises
 function reflect(promise){
     return promise.then(function(v){ return {v:v, status: "fulfilled" }},
                         function(e){ return {e:e, status: "rejected" }});
@@ -24,6 +26,7 @@ fs.readFile('data.json', 'utf8', function(err, contents) {
 
     let data = JSON.parse(contents);
 
+    // For each problem, build a graphql query as a promise
     for (let q of data.stat_status_pairs) {
 
         let title = q.stat.question__title_slug;
@@ -34,7 +37,11 @@ fs.readFile('data.json', 'utf8', function(err, contents) {
         );
     }
 
+    // Wait for all promises then write data to a file
     Promise.all(output.map(reflect)).then(function(values) {
+
+        // Filter out failed promises
+        values = values.filter((x) => x.status == "fulfilled").map((x) => x.v);
 
         fs.writeFile('output.json', JSON.stringify(values), function (err) {
             if (err) throw err;
